752d441b09d06c692039fe15bfebd151565b7827
Ravi Kumar Vykuntham <vykuntham@nexfiniti.in>
2022-03-02 22:58:25 +0530

    boolean

--- javascript/datatypes.js
+++ javascript/datatypes.js
@@ -31,3 +31,26 @@ console.log(exampleString);
 
 const backticks = `Hello with backticks`;
 console.log(backticks);
+
+// numbers
+
+const wholeNumber = 5;
+const decimalNumber = 0.333;
+
+console.log(wholeNumber);
+console.log(decimalNumber);
+
+// booleans
+
+// true - yes, correct, 1
+// false - no, incorrect, 0
+
+const isCool = true;
+
+console.log(typeof isCool);
+
+if (isCool) {
+  console.log("yes it is cool");
+} else {
+  console.log("no, it is not cool");
+}
