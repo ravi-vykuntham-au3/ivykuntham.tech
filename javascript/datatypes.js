5b465d4e12986c48aa5ac0aa131705a566689a56
Ravi Kumar Vykuntham <vykuntham@nexfiniti.in>
2022-03-02 22:51:59 +0530

    datatypes

---
+++ javascript/datatypes.js
@@ -0,0 +1,25 @@
+// Strings
+// Numbers
+// Booleans
+// Null
+// Undefined
+// Object
+// Symbol
+
+// Single line comment
+/* this
+is
+a multi
+line
+comment
+*/
+
+// creating a variable using var keyword
+var variableName = "test";
+
+console.log(variableName);
+
+// variable naming
+// 1. the name should be unique
+// 2. the name should not be any reserved keyword
+// 3. the name must start with a character, _, $.
