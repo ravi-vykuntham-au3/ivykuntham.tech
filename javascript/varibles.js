8971335beba44212ee56fcf5c500af3a84118c8a
Ravi Kumar Vykuntham <vykuntham@nexfiniti.in>
2022-03-02 22:44:01 +0530

    variables

---
+++ javascript/varibles.js
@@ -0,0 +1,18 @@
+// units of storage
+
+// using variables we can perform these operations
+// create
+// store
+// use
+// change it
+
+// var variableName = "Welcome to variables";
+// console.log(variableName);
+
+let variableName = "Welcome to variable";
+variableName = "Hello";
+console.log(variableName);
+
+const variableName2 = "welcome";
+variableName2 = "hello world"; // assignment to constant variable
+console.log(variableName2);
